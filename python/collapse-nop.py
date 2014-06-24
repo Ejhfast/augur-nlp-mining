41b7d4a241378f4964ffabba75ee9a0ff8f0f827
Ethan Fast <ethanfast@rescomp-13-277138.stanford.edu>
2014-06-24 11:13:03 -0700

    folders

---
+++ python/collapse-nop.py
@@ -0,0 +1,11 @@
+import fileinput
+
+count_nop = 0
+
+for line in fileinput.input():
+  if line.strip() == "NOP":
+    count_nop += 1
+  else:
+    print "NOP ", count_nop
+    print line.rstrip()
+    count_nop = 0
