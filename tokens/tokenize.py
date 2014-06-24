da4b398f72e4aaf8414c391d967b55129fc92f97
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-24 09:37:51 -0700

    changes sort to descending instead of ascending

--- tokens/tokenize.py
+++ tokens/tokenize.py
@@ -3,14 +3,18 @@ from collections import defaultdict
 
 counts = defaultdict(int)
 
+"""
 def p((k,v)):
 	print k + '\t' + str(v) 
-
+"""
 for line in fileinput.input():
 	tokens = line.split('\t')
-	actions = '\t'.join(tokens[:-1])
+	actions = tokens[2:]
+	"""
+	if(s in  ["he","she","you","i","we","they"]
 	counts[actions] += 1
 
-counts = sorted(counts.iteritems(), key=counts.get).reverse()
+counts = sorted(counts.iteritems(), key=counts.get)
 counts = filter(lambda (k,v): True if v >1 else False, counts)
 map(p, counts)
+"""
