e937f4204967df6112c29d66302f8c0e7b9d1bd1
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-24 14:22:45 -0700

    changes to add command line options

--- run-reverb.py
+++ run-reverb.py
@@ -5,12 +5,22 @@ import StringIO
 import csv
 import sys
 
-filepath = sys.argv[1]
-numfiles = sys.argv[2]
+numfiles = None
 
-scsv = subprocess.check_output("find " + filepath + " |  grep -e '[4-7][0-9]' | head -" + numfiles + " | java -Xmx512m -jar reverb.jar -f", shell=True) 
+if (len(sys.argv) < 2):
+	raise NameError('Usage: python ' + sys.argv[0] + '.py filepath [numfiles]')
+if (len(sys.argv) > 2):
+	numfiles = sys.argv[2]
+
+filepath = sys.argv[1]
+command = None
+if(numfiles == None):
+	command = "find " + filepath + " |  grep -e '[4-7][0-9]' | java -Xmx512m -jar reverb.jar -f | tee watpad.tsv"
+else:
+	command = "find " + filepath + " |  grep -e '[4-7][0-9]' | head -" + numfiles + " | java -Xmx512m -jar reverb.jar -f | tee watpad.tsv"
+
+scsv = subprocess.check_output(command, shell=True)
 f = StringIO.StringIO(scsv)
 reader = csv.reader(f, delimiter='\t')
 for row in reader:
 	print '\t'.join(row[:5])
-
