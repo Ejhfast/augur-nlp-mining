f98573bef8d0508d1bdc77b698f8f055c9ef77e9
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-24 15:53:03 -0700

    This change cleans up the token extraction pipeline. Conditionals are no longer redundant, and the filtering is done in one file instead of 2.

--- tokenize.py
+++ tokenize.py
@@ -9,12 +9,22 @@ def p((k,v)):
 	if len(k.split('\t')) == 2:
 		print "the person\t" + k + "\t" + str(v) 
 
-pronouns = ["he","she","you","i","we","they"]
+#subject match
+#TODO: automated subject match
+subject_match = ["i", "you", "he", "she", "it", "we", "you", "they"]
+
+#object match
+#TODO: automated object match
+object_match = ["me", "you", "him", "her", "us", "them", "#"]
+
 for line in fileinput.input():
 	tokens = line.split('\t')
 	actions = tokens[2:]
-	if any(s == actions[0] for s in pronouns):
-		counts['\t'.join(actions[1:]).rstrip()] += 1
+	if any(any(r == s for r in subject_match) for s in actions[0].split(' ')):
+		if not any(any(r == s for r in object_match) for s in actions[2].split(' ')):
+			k = '\t'.join(actions[1:]).rstrip()
+			counts[k] += 1
+			p((k, 1))
 
 counts = sorted(counts.iteritems(), key=operator.itemgetter(1), reverse= True)
 map(p, counts)
