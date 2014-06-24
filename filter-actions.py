61cd1cf7a4ac8c56a1aa67319aa4fa06f66fff7c
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-24 14:05:13 -0700

    gets replace list to be the same

---
+++ filter-actions.py
@@ -0,0 +1,29 @@
+import fileinput
+
+# The list of approved tokens. See workflow.txt
+APPROVED_LIST = "tokens/approved-combined.tsv"
+
+
+whitelist = {}
+for line in open(APPROVED_LIST):
+	whitelist[' '.join(line.split('\t')[:-1]).strip()]= True
+
+def replace(word):
+  #TODO: see why this filter different from that in filter-actions.py
+  singular = ["he", "she", "him", "i", "myself", "you", "me", "himself", "herself", "yourself"]
+  plural = ["they", "them", "we", "us", "ourselves", "themselves"]
+  if any(s == word for s in singular):
+    return "the person"
+  elif any(s == word for s in plural):
+    return "the people"
+  return word
+
+for line in fileinput.input():
+  tokens = line.split('\t')
+  header = tokens[:2]
+  relations = tokens[2:]
+  relations = map(lambda x: replace(x.strip()), relations)
+  if whitelist.get(' '.join(relations).strip()) == True:
+    print  "\t".join(header + relations)
+  else:
+    print "NOP"
