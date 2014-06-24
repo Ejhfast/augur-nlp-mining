51cc863c3fa23fda66b9f265aa0c989be7235a36
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-24 09:57:27 -0700

    sucessfully ports tokenize to python

--- tokens/tokenize.py
+++ tokens/tokenize.py
@@ -1,20 +1,19 @@
 import fileinput
+import operator
 from collections import defaultdict
 
 counts = defaultdict(int)
 
-"""
 def p((k,v)):
-	print k + '\t' + str(v) 
-"""
+	print "the person\t" + k + "\t" + str(v) 
+
+pronouns = ["he","she","you","i","we","they"]
 for line in fileinput.input():
 	tokens = line.split('\t')
 	actions = tokens[2:]
-	"""
-	if(s in  ["he","she","you","i","we","they"]
-	counts[actions] += 1
+	if any(s == actions[0] for s in pronouns):
+		counts['\t'.join(actions[1:]).rstrip()] += 1
 
-counts = sorted(counts.iteritems(), key=counts.get)
-counts = filter(lambda (k,v): True if v >1 else False, counts)
+#TODO reverse?
+counts = sorted(counts.iteritems(), key=operator.itemgetter(1), reverse= True)
 map(p, counts)
-"""
