a9820ea11a44ee2c690cb55c655a05df739b3b0a
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-24 15:20:06 -0700

    finds and fixes bug relating to encoding issues

--- tokenize.py
+++ tokenize.py
@@ -5,7 +5,9 @@ from collections import defaultdict
 counts = defaultdict(int)
 
 def p((k,v)):
-	print "the person\t" + k + "\t" + str(v) 
+	#TODO: better way to do this? 
+	if len(k.split('\t')) == 2:
+		print "the person\t" + k + "\t" + str(v) 
 
 pronouns = ["he","she","you","i","we","they"]
 for line in fileinput.input():
@@ -14,6 +16,5 @@ for line in fileinput.input():
 	if any(s == actions[0] for s in pronouns):
 		counts['\t'.join(actions[1:]).rstrip()] += 1
 
-#TODO reverse?
 counts = sorted(counts.iteritems(), key=operator.itemgetter(1), reverse= True)
 map(p, counts)
