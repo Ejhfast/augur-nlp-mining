045da199035e421a827d789a1f6622417dbcad9d
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-24 16:48:35 -0700

    fixes bug relating replace function

--- filter-actions.py
+++ filter-actions.py
@@ -5,33 +5,23 @@ import fileinput
 def approve(actions):
   #subject match
   #TODO: automated subject match
-  subject_match = ["i", "you", "he", "she", "it", "we", "you", "they"]
+  subject_match = ["i", "you", "he", "she", "we", "they"]
 
   #object match
-  #TODO: automated object match
-  object_match = ["me", "you", "him", "her", "us", "them", "#"]
+  #TODO: automated object match. Problems found with approach of object matching
+  #object_match = ["me", "you", "him", "her", "us", "them", "#", "i", "you", "he", "she", "we", "they"]
 
   if any(any(r == s for r in subject_match) for s in actions[0].split(' ')):
-    if not any(any(r == s for r in object_match) for s in actions[2].split(' ')):
-      #k = '\t'.join(actions[1:]).rstrip()
-      return True
+    #if not any(any(r == s for r in object_match) for s in actions[2].split(' ')):
+    return True
   return False
-
-def replace(word):
-  singular = ["he", "she", "him", "i", "myself", "you", "me", "himself", "herself", "yourself"]
-  plural = ["they", "them", "we", "us", "ourselves", "themselves"]
-  if any(s == word for s in singular):
-    return "the person"
-  elif any(s == word for s in plural):
-    return "the people"
-  return word
 
 for line in fileinput.input():
   line = line.decode("ascii", "ignore")
   tokens = line.split('\t')
   header = tokens[:2]
   actions = tokens[2:]
-  actions = map(lambda x: replace(x.strip()), actions)
+  #actions = map(lambda x: replace(x.strip()), actions)
   if approve(actions):
     print "\t".join(header + actions)
   else:
