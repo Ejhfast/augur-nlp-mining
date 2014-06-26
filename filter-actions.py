c85c24740c959686dc34d61e94deadc6ba040932
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-26 09:16:57 -0700

    speedups

--- filter-actions.py
+++ filter-actions.py
@@ -13,23 +13,26 @@ def approve(actions):
 
   #TODO explore if the same subject or different subject match in actions[0] and actions[2]
   #Todo replace proper nouns with pronoun
-  if any(any(r == s for r in subject_match) for s in actions[0].split(' ')):
-    if not any(any(r == s for r in object_match) for s in actions[2].split(' ')):
+  if len(set(subject_match).intersection(actions[0].split(' '))) > 0:
+    if len(set(object_match).intersection(actions[2].split(' '))) == 0:
       return True
   return False
 
-APPROVED_LIST = "approved-combined.tsv"
-whitelist = {}
-for line in open(APPROVED_LIST):
-  whitelist[' '.join(line.split('\t')[:-1]).strip()]= True
+def run():
+  APPROVED_LIST = "approved-combined.tsv"
+  whitelist = {}
+  for line in open(APPROVED_LIST):
+    whitelist[' '.join(line.split('\t')[1:-1]).rstrip()]= True
 
-for line in fileinput.input():
-  line = line.decode("ascii", "ignore")
-  actions = line.split('\t')[2:]
-  actions = map(lambda x: x.strip(), actions)
-  if approve(actions):
-    output = "the person " + " ".join(actions[1:]).rstrip()
-    if whitelist.get(output) == True:
-      print output
-  else:
-    print "NOP"
+  for line in fileinput.input():
+    line = line.decode("ascii", "ignore")
+    actions = line.split('\t')[2:]
+    if approve(actions):
+      verbobject = " ".join(actions[1:]).rstrip()
+      if whitelist.get(verbobject) == True:
+        output = "the person " + verbobject
+        print output
+    else:
+      print "NOP"
+
+run()
