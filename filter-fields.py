a75ab0d129a6f5d23d54a74ae67b0ea75c67a913
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-25 23:29:35 -0700

    renamed filterfields to filter-fields and added ascii decodable check

--- filter-fields.py
+++ filter-fields.py
@@ -2,5 +2,6 @@ import fileinput
 import csv
 
 for line in fileinput.input():
+	line = line.decode("ascii", "ignore")
 	row = line.split('\t')
-	print '\t'.join(row[-3:])
+	print '\t'.join(row[-3:]).rstrip()
