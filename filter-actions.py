64789b0053c2ed2729fe5afa2ba38f07d9e06f79
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-26 16:34:37 -0700

    fixes a bug with object match (.strip() had to be called first)

--- filter-actions.py
+++ filter-actions.py
@@ -2,19 +2,12 @@ from __future__ import print_function
 import fileinput
 import sys
 import warnings
+
 def approve(actions):
-  #subject match
-  #TODO: automated subject match
   subject_match = ["i", "you", "he", "she", "we", "they"]
-
-  #object match
-  #TODO: automated object match. Problems found with approach of object matching
-  object_match = ["me", "you", "him", "her", "us", "them", "#", "i", "you", "he", "she", "we", "they"]
-
-  #TODO explore if the same subject or different subject match in actions[0] and actions[2]
-  #Todo replace proper nouns with pronoun
-  if len(set(subject_match).intersection(actions[0].split(' '))) > 0:
-    if len(set(object_match).intersection(actions[2].split(' '))) == 0:
+  object_match = ["me", "you", "him", "his", "her", "us", "them", "#", "i", "you", "he", "she", "we", "they", "it"]
+  if len(set(subject_match).intersection(actions[0].split())) > 0:
+    if len(set(object_match).intersection(actions[2].split())) == 0:
       return True
   return False
 
@@ -30,8 +23,8 @@ def run():
 
   for line in fileinput.input():
     total = total + 1
-    line = line.decode("ascii", "ignore")
-    actions = line.split('\t')
+    line = line.decode("ascii", "ignore").rstrip()
+    actions = map(lambda x: ' '.join(x.split()), line.split('\t'))
     if(len(actions) == 5):
       actions = actions[2:]
     if(len(actions) != 3):
@@ -48,4 +41,5 @@ def run():
       count_nop += 1
     if(total%100000 ==0):
       print("Processed " + str(total) + " input lines. " + str(discarded) + " discarded", file=sys.stderr,  end="\r")
+
 run()
