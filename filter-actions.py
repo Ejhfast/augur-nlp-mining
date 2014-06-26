173f70730b014ad8f3a86ada3c372e630d55fa86
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-26 11:17:18 -0700

    nop-collapse integrated into filter-actions

--- filter-actions.py
+++ filter-actions.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import fileinput
 import sys
-
+import warnings
 def approve(actions):
   #subject match
   #TODO: automated subject match
@@ -18,26 +18,34 @@ def approve(actions):
       return True
   return False
 
+
 def run():
+  total = 0
+  count_nop = 0
+  discarded = 0
   APPROVED_LIST = "files/approved-combined.tsv"
   whitelist = {}
   for line in open(APPROVED_LIST):
     whitelist[' '.join(line.split('\t')[1:-1]).rstrip()]= True
 
   for line in fileinput.input():
+    total = total + 1
     line = line.decode("ascii", "ignore")
     actions = line.split('\t')
     if(len(actions) == 5):
       actions = actions[2:]
     if(len(actions) != 3):
-      print ("Warning: Found token with less than 3 parts", file=sys.stderr)
+      discarded = discarded + 1
       continue
     if approve(actions):
       verbobject = " ".join(actions[1:]).rstrip()
       if whitelist.get(verbobject) == True:
         output = "the person " + verbobject
+        print("NOP ", count_nop)
         print(output)
+        count_nop = 0
     else:
-      print("NOP")
-
+      count_nop += 1
+    if(total%100000 ==0):
+      print("Processed " + str(total) + " input lines. " + str(discarded) + " discarded", file=sys.stderr,  end="\r")
 run()
