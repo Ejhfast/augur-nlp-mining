248479f7f6b338aded3d689286cef762de64ee24
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-27 11:19:44 -0700

    implements working word whitelist program

--- process/filter-actions.py
+++ process/filter-actions.py
@@ -65,27 +65,22 @@ def run():
     total = total + 1
     
     [line1, line2] = map(lambda line: line.decode("ascii", "ignore").rstrip(), [line1, line2])
-    [actions1, actions2] = map(lambda line: map(lambda x: ' '.join(x.split()), line.split('\t'), [line1, line2])
-    
-    """
-    todo : readd
-    if(len(actions) == 5):
-      actions = actions[2:]
-    if(len(actions) != 3):
+    [actions1, actions2] = map(lambda line: map(lambda x: ' '.join(x.split()), line.split('\t')), [line1, line2])
+    [actions1, actions2] = map(lambda actions : actions[2:] if(len(actions) == 5) else actions, [actions1, actions2])
+    if any(map(lambda actions: len(actions) != 3, [actions1, actions2])):
       discarded = discarded + 1
       continue
-    """
-    if approve(actions):
-      #add last line
-      verbobject = " ".join(actions[1:]).rstrip()
-      #
-      if len(set(actions[2].split()).intersection(wl)) > 0:
+    if all(map(lambda actions: approve(actions), [actions1, actions2])):
+      [verbobject1, verbobject2] = map(lambda actions:" ".join(actions[1:]).rstrip(), [actions1, actions2])
+      #todo : any vs all
+      if any(map(lambda actions: len(set(actions[2].split()).intersection(wl)) > 0, [actions1, actions2])):
       #if whitelist.get(verbobject) == True:
-        output = "the person " + verbobject
+        [output1, output2] = map(lambda verbobject: "the person " + verbobject, [verbobject1, verbobject2])
         print("NOP ", count_nop)
-        print(output)
-        count_nop = 1
-      last_approved = verbobject
+        print(output1)
+        print("NOP ", 0)
+        print(output2)
+        count_nop = 0
     else:
       count_nop += 1
     if(total%100000 ==0):
