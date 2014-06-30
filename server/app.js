22cc6ad8ee5edefb79e29d8d6c46e1ddb526b1de
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-06-30 10:37:32 -0700

    changes to make 'node app.js' work

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
-var express = require('express');
+
+/*var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -31,6 +35,8 @@ app.use(function(req, res, next) {
     next(err);
 });
 
+
+
 /// error handlers
 
 // development error handler
@@ -55,5 +61,65 @@ app.use(function(err, req, res, next) {
     });
 });
 
+// Create an HTTPS service identical to the HTTP service.
+var server = http.Server(app)
+//var server = http.createServer(app)
+
+var io = require('socket.io').listen(server);
+server.listen(app.get('port'));
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 
 module.exports = app;
+*/
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var path = require('path');
+var express = require('express')
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.set('port', process.env.PORT || 3000);
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen( app.get('port'), function(){
+    console.log("Server listening on " + app.get('port'))
+});
+
+
+
+app.use('/', routes);
+app.use('/users', users);
+/*
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+*/
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
