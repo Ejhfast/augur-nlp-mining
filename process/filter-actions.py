809dfe7db25ffbb371535c13518249acd4292818
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-07-02 15:50:44 -0700

    fixes order bug

--- process/filter-actions.py
+++ process/filter-actions.py
@@ -6,63 +6,64 @@ import sys
 import itertools
 from itertools import tee, izip
 
-"""Todos: 
-    Make work with tsv whitelists
-    Replace possessive pronouns with 'the'
-    Implement closing the file
-    Explore second action constraints
-"""
-
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b= tee(iterable)
-    next(a, None)
+    next(b, None)
     return izip(a, b)
 
-
-def approve(actions):
-  subject_match = ["i", "you", "he", "she", "we", "they"]
-  object_match = ["me", "you", "him", "his", "her", "us", "them", "#", "i", "you", "he", "she", "we", "they", "it"]
-  if len(set(subject_match).intersection(actions[0].split())) > 0:
-    if len(set(object_match).intersection(actions[2].split())) == 0:
-      return True
+subjects = set(["i", "you", "he", "she", "we", "they"])
+  
+def subject_match(actions):
+  if len(subjects.intersection(actions[0].split())) > 0:
+    return True
   return False
 
+objects = set(["me", "you", "him", "his", "her", "us", "them", "#", "i", "you", "he", "she", "we", "they", "it"])
+  
+def object_match(actions):
+  if len(objects.intersection(actions[2].split())) == 0:
+    return True
+  return False
+
+def approve(actions):
+  return all([subject_match(actions), object_match(actions)])
 
 def fillinput():
   inp = None
   inf = None
-  iswhitelisttsv = None
+  nowhitelist = False
+  isTsv = False
   if len(sys.argv) == 3:
     inp = open(sys.argv[1])
     inf = open(sys.argv[2])
+    if any(map(lambda word: word in sys.argv[2], ['.tsv'])):
+      isTsv = True
   elif len(sys.argv) == 2:
-    if any(map(lambda word: word in sys.argv[1], ['.txt', 'approved', 'whitelist'])):
       inf = open(sys.argv[1])
       inp = sys.stdin
-    else:
-      inp = open(sys.argv[1])
-      iswhitelisttsv = True
+      if any(map(lambda word: word in sys.argv[1], ['.tsv'])):
+        isTsv = True
   elif len(sys.argv) == 1:
     inp = sys.stdin
-    iswhitelisttsv = True
+    nowhitelist = True
   else:
     exit(1)
 
-  return (inp, inf, iswhitelisttsv)
+  return (inp, inf, nowhitelist, isTsv)
 
 def run():
   total = 0
-  count_nop = 0
+  count_nop = 1
   discarded = 0
-  (inp, inf, iswhitelisttsv) = fillinput()
   whitelist = {}
-  if(iswhitelisttsv):
-    APPROVED_LIST = "../files/approved-combined.tsv"
-    for line in open(APPROVED_LIST):
-      whitelist[' '.join(line.split('\t')[1:-1]).rstrip()]= True
-  else:
-      whitelist = inf.read().split()
+  (inp, inf, nowhitelist, isTsv) = fillinput()
+  if not nowhitelist:
+    if isTsv:
+      for line in inf:
+        whitelist[' '.join(line.split('\t')[1:-1]).rstrip()]= True
+    else:
+      whitelist = set(inf.read().split())
 
   for line1, line2 in pairwise(inp):  
     total = total + 1
@@ -74,16 +75,17 @@ def run():
       continue
     if all(map(lambda actions: approve(actions), [actions1, actions2])):
       [verbobject1, verbobject2] = map(lambda actions:" ".join(actions[1:]).rstrip(), [actions1, actions2])
-      if (iswhitelisttsv and whitelist.get(verbobject1) == True) or (not iswhitelisttsv and len(set(verbobject1.split()).intersection(whitelist)) > 0):
-        [output1, output2] = map(lambda verbobject: "the person " + verbobject, [verbobject1, verbobject2])
+      if nowhitelist or (not isTsv and len(whitelist.intersection(verbobject1.split()))> 0) or (isTsv and whitelist.get(verbobject1) == True):
+        [output1, output2] = map(lambda verbobject: verbobject, [verbobject1, verbobject2])
         print("NOP ", count_nop)
         print(output1)
-        print("NOP ", 0)
+        print("NOP ", 1)
         print(output2)
-        count_nop = 0
+        count_nop = 1
     else:
       count_nop += 1
     if(total%100000 ==0):
       print("Processed " + str(total) + " input lines. " + str(discarded) + " discarded", file=sys.stderr,  end="\r")
-
+  inp.close()
+  inf.close()
 run()
