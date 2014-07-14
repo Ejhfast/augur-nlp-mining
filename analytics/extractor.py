389ce5268aef8b1efbb2617329612790354a5663
Ethan Fast <ethanfast@rescomp-13-282343.stanford.edu>
2014-07-14 13:35:00 -0700

    some genre based analysis

--- analytics/extractor.py
+++ analytics/extractor.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
-from collections import defaultdict
+from collections import defaultdict, Counter, deque
+import multiprocessing as mp
 import nltk
 import fileinput
 import itertools
@@ -16,6 +17,9 @@ def each_cons(xs, n):
 def print_dict(counts,n):
 	top = sorted(counts.iteritems(), key=operator.itemgetter(1), reverse=True)
 	return "\n".join([k+"\t"+str(v) for k,v in itertools.islice(top,0,n)])
+def grouper(n, iterable, fillvalue=None):
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
 def out_error(string, clear=True):
 	if clear: sys.stderr.write("\x1b[2J\x1b[H")
 	print(string, file=sys.stderr)
@@ -31,8 +35,13 @@ def list_files(path):
 # file -> line
 def iter_lines(iter):
   for file in iter:
-    with open(file,"r") as f:
-      for line in f: yield line
+    if file: # Stupid None from grouper...
+      with open(file,"r") as f:
+        for line in f: yield line
+
+def iter_lines_single(file):
+  with open(file,"r") as f:
+    for line in f: yield line
 
 # line -> words
 def iter_words(iter):
@@ -55,26 +64,37 @@ def count_items(iter):
     yield table
 
 # lines -> [words] in lines with that contain word_set items
-def word_window(word_set):
+def has_words(words):
+  # word_set = set(words)
+  # set_len = len(word_set)
   def window(iter):
-    for line in iter:
-      words = [w.lower() for w in line.rstrip().split() if w.lower() in nouns]
-      #print(nouns)
-      print([line,words])
-      if(len(word_set.intersection(set(words))) > 2):
-        for w in words: yield w
-          # if(not blacklist[w]): yield w
+    for word_seq in iter:
+      if(reduce(operator.and_, [word_seq[i] == a for i,a in enumerate(words)])):
+        yield word_seq
+      # if(len(word_set.intersection(set(word_seq))) >= set_len):
+      #   yield word_seq
   return window
 
-test_set = set(["plate","fork","napkin","chair"])
+BLOCK_SIZE = 1000
 
-pipeline = pipe([iter_lines, iter_words, n_grams(3), count_items], list_files(sys.argv[1]))
+def pipeline(i,file_list):
+  process = pipe([iter_lines, iter_words, n_grams(3), count_items], file_list)
+  inter_res = None
+  for inter_res in process: pass
+  out_error("Finished through: {}".format(i*BLOCK_SIZE))
+  return inter_res
 
-count, last_count = 0, None
-for res in pipeline:
-  count += 1
-  last_count = res
-  if(count%100000==0):
-    window = print_dict(res,50)
-    out_error(window)
-print(print_dict(last_count,None))
+def list_files_multi(path):
+  pool = mp.Pool(processes=4)
+  file_blocks = grouper(BLOCK_SIZE,list_files(path))
+  dicts = [pool.apply_async(pipeline, args=(i,f_seq)) for i,f_seq in enumerate(file_blocks)]
+  out = [p.get() for p in dicts]
+  out_error("Reducing dictionaries...")
+  final = defaultdict(int)
+  for i,d in enumerate(out):
+    out_error("Merging {}".format(i))
+    if d:
+      for k,v in d.iteritems(): final[k] += v
+  print(print_dict(final,None))
+
+list_files_multi(sys.argv[1])
