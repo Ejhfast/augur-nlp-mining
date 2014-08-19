79d489178f57f1672572293eb5d1436873f7b901
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-08-19 13:31:32 -0700

    removes old files

--- whitelist/scrape.py
+++
@@ -1,6 +0,0 @@
-import urllib2
-
-request = urllib2.Request("http://www.enchantedlearning.com/wordlist/householddevices.shtml")
-response = urllib2.urlopen(request)
-the_page = response.read()
-print the_page
